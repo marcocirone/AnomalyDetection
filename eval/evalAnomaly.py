2ed66ec4566da965aa6ef671eef8fa1bf17adc64
MarcoCirone <marcocirone00@outlook.it>
2024-05-20 16:50:19 +0200

    fix evalAnomaly

--- eval/evalAnomaly.py
+++ eval/evalAnomaly.py
@@ -8,13 +8,28 @@ from PIL import Image
 import numpy as np
 from erfnet import ERFNet
 from erfnet_quantized import Net
-from train.quantization import quantize_model, load_quant_dict
 import os.path as osp
 from argparse import ArgumentParser
+from torchvision.transforms import Compose, Resize, ToTensor
 from ood_metrics import fpr_at_95_tpr, calc_metrics, plot_roc, plot_pr,plot_barcode
 from sklearn.metrics import roc_auc_score, roc_curve, auc, precision_recall_curve, average_precision_score
+from matplotlib import pyplot as plt
 
 seed = 42
+
+input_transform = Compose(
+    [
+        Resize((512, 1024), Image.BILINEAR),
+        ToTensor(),
+        # Normalize([.485, .456, .406], [.229, .224, .225]),
+    ]
+)
+
+target_transform = Compose(
+    [
+        Resize((512, 1024), Image.NEAREST),
+    ]
+)
 
 # general reproducibility
 random.seed(seed)
@@ -62,9 +77,9 @@ def main():
 
     if args.loadModel == 'erfnet.py':
         model = ERFNet(NUM_CLASSES)
-    elif args.loadMmodel == 'quantized_erfnet.py':
-        model = Net(NUM_CLASSES)
-        model = quantize_model(model, args, False)
+    # elif args.loadMmodel == 'quantized_erfnet.py':
+    #     model = Net(NUM_CLASSES)
+    #     model = quantize_model(model, args, False)
 
     if (not args.cpu):
         model = torch.nn.DataParallel(model).cuda()
@@ -83,8 +98,8 @@ def main():
         return model
     if args.loadModel == 'erfnet.py':
         model = load_my_state_dict(model, torch.load(weightspath, map_location=lambda storage, loc: storage))
-    elif args.loadModel == 'quantized_erfnet.py':
-        model = load_quant_dict(model, torch.load(weightspath, map_location=lambda storage, loc: storage))
+    # elif args.loadModel == 'quantized_erfnet.py':
+    #     model = load_quant_dict(model, torch.load(weightspath, map_location=lambda storage, loc: storage))
     print ("Model and weights LOADED successfully")
     model.eval()
     
@@ -106,7 +121,7 @@ def main():
               softmax_probs = torch.nn.functional.softmax(result.squeeze(0)/temperature, dim=0)
               anomaly_result = 1.0 - np.max(softmax_probs.data.cpu().numpy(), axis=0)
           elif args.method == "max_logit":
-              anomaly_result = 1.0 - np.max(result.squeeze(0).data.cpu().numpy(), axis=0)
+              anomaly_result = -np.max(result.squeeze(0).data.cpu().numpy(), axis=0)
           elif args.method == "max_entropy":
               # softmax_probs = torch.nn.functional.softmax(result.squeeze(0), dim=0)
               # anomaly_result = torch.div(-torch.sum(softmax_probs * torch.nn.functional.log_softmax(result.squeeze(0), dim=0), dim=0), torch.log(torch.tensor(result.shape[1]))).data.cpu().numpy()
@@ -191,22 +206,29 @@ def main():
 
     for path in glob.glob(os.path.expanduser(str(args.input[0]))):
           print(path)
-          images = torch.from_numpy(np.array(Image.open(path).convert('RGB'))).unsqueeze(0).float()
-          images = images.permute(0,3,1,2)
+          images = input_transform((Image.open(path).convert('RGB'))).unsqueeze(0).float()
+        #   images = torch.from_numpy(np.array(Image.open(path).convert('RGB'))).unsqueeze(0).float()
+        #   images = images.permute(0,3,1,2)
           with torch.no_grad():
-              result = model(images)
+              result = model(images).squeeze(0)
+          print(result.shape)
+          result = result[:-1]
+          print(result.shape)
           if args.method == "msp":
               temperature = float(args.temperature)
               softmax_probs = torch.nn.functional.softmax(result.squeeze(0)/temperature, dim=0)
               anomaly_result = 1.0 - np.max(softmax_probs.data.cpu().numpy(), axis=0)
           elif args.method == "max_logit":
-              anomaly_result = 1.0 - np.max(result.squeeze(0).data.cpu().numpy(), axis=0)
+              anomaly_result = -torch.max(result, dim=0)[0]
+              anomaly_result = anomaly_result.data.cpu().numpy()
+            #   anomaly_result = -np.max(result.squeeze(0).data.cpu().numpy(), axis=0)
           elif args.method == "max_entropy":
               # softmax_probs = torch.nn.functional.softmax(result.squeeze(0), dim=0)
               # anomaly_result = torch.div(-torch.sum(softmax_probs * torch.nn.functional.log_softmax(result.squeeze(0), dim=0), dim=0), torch.log(torch.tensor(result.shape[1]))).data.cpu().numpy()
               softmax_probs = torch.nn.functional.softmax(result.squeeze(0), dim=0)
               log_softmax_probs = torch.nn.functional.log_softmax(result.squeeze(0), dim=0)
               anomaly_result = torch.div(-torch.sum(softmax_probs * log_softmax_probs, dim=0),torch.log(torch.tensor(result.shape[1]))).data.cpu().numpy()
+        #   anomaly_result = anomaly_result.data.cpu().numpy()
           pathGT = path.replace("images", "labels_masks")                
           if "RoadObsticle21" in pathGT:
             pathGT = pathGT.replace("webp", "png")
@@ -215,7 +237,9 @@ def main():
           if "RoadAnomaly" in pathGT:
             pathGT = pathGT.replace("jpg", "png")  
 
+
           mask = Image.open(pathGT)
+          mask = target_transform(mask)
           ood_gts = np.array(mask)
 
           if "RoadAnomaly" in pathGT:
@@ -229,6 +253,47 @@ def main():
               ood_gts = np.where((ood_gts==14), 255, ood_gts)
               ood_gts = np.where((ood_gts<20), 0, ood_gts)
               ood_gts = np.where((ood_gts==255), 1, ood_gts)
+
+          def select_treshold(proba, target):
+                precision, recall, thresholds = precision_recall_curve(target, proba)
+                best_treshold = sorted(
+                    list(zip(np.abs(precision - recall), thresholds)), key=lambda i: i[0], reverse=False
+                )[0][1]
+
+                return best_treshold
+
+          labels = np.where(ood_gts.flatten() != 1, -1, ood_gts.flatten())
+        #   print(ood_gts.flatten())
+        #   print(labels)
+          results = anomaly_result.flatten()
+          best_treshold = select_treshold(results, labels)
+          save_path = f'.\\save_color\\{args.method}\\' + path.split('Validation_Dataset\\')[2]
+          save_path2 = f'.\\save_color\\{args.method}\\gt\\' + path.split('Validation_Dataset\\')[2]
+          if not os.path.exists("\\".join(save_path.split("\\")[:-1:])):
+              os.makedirs("\\".join(save_path.split("\\")[:-1:]))
+          if not os.path.exists("\\".join(save_path2.split("\\")[:-1:])):
+              os.makedirs("\\".join(save_path2.split("\\")[:-1:]))
+
+        #   print(f"Threshold: {best_treshold}")
+        #   print(f"Scores: {anomaly_result}")
+
+          norm_an_res = (anomaly_result - np.min(anomaly_result)) / (np.max(anomaly_result) - np.min(anomaly_result))
+          norm_th = (best_treshold - np.min(anomaly_result)) / (np.max(anomaly_result) - np.min(anomaly_result)) 
+
+        # USED TO SAVE THE IMAGES
+        #   plt.imsave(
+        #         save_path,
+        #         norm_an_res - norm_th,
+        #         cmap='bwr',
+        #   )
+
+        # #   print(ood_gts)
+
+        #   plt.imsave(
+        #         save_path2,
+        #         (ood_gts == 1) * 255,
+        #         cmap='bwr',
+        #   )
 
           if 1 not in np.unique(ood_gts):
               continue              
